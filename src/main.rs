b2e8f547fe8d7b59784fa5c6c608f800b2e41acd
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-08-30 18:12:41 +0100

    Basic ratatui program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers, KeyEventState};
+use ratatui::{text::Text, Frame};
+
 fn main() {
-    println!("Hello, world!");
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("Failed to draw frame");
+        if matches!(
+            event::read().expect("Failed to read event"), 
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE
+            })
+        ) {
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello from Lebhaft! <3");
+    frame.render_widget(text, frame.area());
 }
