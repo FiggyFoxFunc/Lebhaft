54c2a31daa5937bbeb484d4758c77f12e9094da6
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-09-03 21:50:01 +0100

    Adding list pop up.

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
-use ratatui::{DefaultTerminal, Frame};
+use ratatui::{DefaultTerminal, Frame, style::Stylize, symbols::border, text::Line, widgets::Block};
 
 use crate::ui::MainMenuWidget;
 
@@ -31,7 +31,14 @@ impl App {
     }
 
     pub fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(MainMenuWidget, frame.area());
+        let title = Line::from("Welcome to Lebhaft!").centered().magenta();
+        let title_bottom = Line::from("Your personal music player and organiser").centered().magenta();
+        let block = Block::bordered()
+            .title(title)
+            .title_bottom(title_bottom)
+            .border_set(border::THICK);
+
+        frame.render_widget(block, frame.area());
     }
 
     pub fn handle_events(&mut self) {
--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -1,15 +1,23 @@
-use ratatui::{buffer::Buffer, layout::Rect, style::Stylize, symbols::border, text::Line, widgets::{Block, Paragraph, Widget}};
+use ratatui::widgets::{ListState, StatefulWidget};
 
-pub struct MainMenuWidget;
+pub struct MainMenuWidget {
+    items: Vec<String>,
+    state: ListState
+}
 
-impl Widget for MainMenuWidget {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from("Welcome to Lebhaft!").centered().magenta();
-        let title_bottom = Line::from("Your personal music player and organiser").centered().magenta();
-        let block = Block::bordered()
-            .title(title)
-            .title_bottom(title_bottom)
-            .border_set(border::THICK);
-        let _ = Paragraph::new("Still a work in progress...").centered().block(block).render(area, buf);
-    }   
+impl MainMenuWidget {
+    pub fn new() -> Self {
+        Self { 
+            items: vec!["Open Player".into(), "Settings".into(), "Exit".into()], 
+            state: ListState::default() 
+        }
+    }
+}
+
+impl StatefulWidget for MainMenuWidget {
+    type State = ListState;
+
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State) {
+        todo!();
+    }
 }
